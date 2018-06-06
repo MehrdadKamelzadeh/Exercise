a738a52cedffdd63b9895c32201b3c31c3192622
Mehrdad Kamelzadeh <mehrdad.kamelzadeh@gmail.com>
2018-06-06 17:40:37 +0430

    Quicksort

---
+++ Algorithms/MyQuickSort.cs
@@ -0,0 +1,60 @@
+﻿namespace Algorithms
+{
+    public class MyQuickSort
+    {
+
+        public void DoQuickSort(int[] arr, int left, int right)
+        {
+            var pivot = Partition(arr, left, right);
+
+            if (left < right)
+            {
+                if (pivot > 1)
+                {
+                    DoQuickSort(arr, left, pivot - 1);
+                }
+
+                if (pivot + 1 < right)
+                {
+                    DoQuickSort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private int Partition(int[] arr, int left, int right)
+        {
+            var pivot = arr[left];
+
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left == right)
+                {
+                    return right;
+                }
+                if (left < right)
+                {
+                    Swap(arr, left, right);
+                }
+
+            }
+        }
+
+        private void Swap(int[] arr, int left, int right)
+        {
+            var temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var qs = new MyQuickSort(); 
+            int[] arr = new int[] { 4, 6, 1, 2, 5, 8 };
+            qs.DoQuickSort(arr, 0, arr.Length - 1);
+        }
+    }
+}
